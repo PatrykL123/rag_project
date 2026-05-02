59a18d492c31b3ac0b74b252a4bd9af9adcd550e
Patryk <patryk_lewandowski12@interia.pl>
2026-05-02 14:57:58 +0200

    Adding get_new_files function to file_tracker.py which scans a folder to find files to process

--- src/data/file_tracker.py
+++ src/data/file_tracker.py
@@ -1,6 +1,7 @@
 import hashlib 
 import json
 import os 
+import glob 
 
 def hash_file(file_path):
     hasher = hashlib.sha256()
@@ -15,5 +16,44 @@ def hash_file(file_path):
     return hasher.hexdigest()
 
 
+def get_new_files(directory_path: str, tracker_file: str ="src/data/processed_files.json") -> list:
+
+
+    """
+    Scans a folder and returns a list of paths to PDF files
+    that are new or have been modified since the last run.
+    """
+
+    try:
+        with open(tracker_file, 'r') as f:
+            processed_files = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        processed_files = {}
     
+    files_to_process = []
+
+    files_found = glob.glob(f"{directory_path}/*.pdf")
+
+    if not files_found:
+        print("No files found")
+        return []
+
+
+    for filename in files_found: 
+
+        file_hash = hash_file(filename)
+
+        if filename not in processed_files or processed_files[filename] != file_hash:
+
+            files_to_process.append(filename)
+
+    return files_to_process
+
+
+
+
+
+
+        
+
     
