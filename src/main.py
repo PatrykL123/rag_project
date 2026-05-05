ccf1e02e3f900afe7f519f60f4fea37eadabf9c3
Patryk <patryk_lewandowski12@interia.pl>
2026-05-05 15:50:15 +0200

    feat: implement working CLI chat interface for RAG main.py

---
+++ src/main.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import os
+
+from src.rag.generator import generate_answer
+
+def main():
+    load_dotenv()
+
+    if os.environ.get("OPENAI_API_KEY"):
+        print("Openai API key is set\n\n")
+    else:
+        print("please provide openai API key in .env file")
+        exit()
+
+    print("If you want to exit the chat write: exit\n\n")
+
+    while True:
+
+        query = input("What is your question?: ")
+
+        if not query.strip():
+            continue
+
+        if query.lower() == "exit":
+            break
+        else:
+
+            answer = generate_answer(query)
+            print(f"\n\nAnswer: {answer}")
+            print("-" * 40)
+
+
+if __name__ == "__main__":
+    main()
+
--- src/pipeline/ingestion.py
+++ src/pipeline/ingestion.py
@@ -8,17 +8,17 @@ from src.pipeline.file_tracker import get_new_files, update_tracker
 from src.rag.vector_store import add_to_vector_store, delete_vectors_by_source
 from src.processing.chunking import load_and_split_documents
 
-load_dotenv()
-
-if os.environ.get("OPENAI_API_KEY"):
-    print("OPENAI_API_KEY is set.")
-else:
-    raise ValueError("Brak klucza OPENAI_API_KEY w pliku .env!")
-
-
 
 
 if __name__ == "__main__":
+
+    load_dotenv()
+
+    if os.environ.get("OPENAI_API_KEY"):
+        print("OPENAI_API_KEY is set.")
+    else:
+        raise ValueError("Brak klucza OPENAI_API_KEY w pliku .env!")
+
     raw_data = "data/raw"
     new_files = get_new_files(raw_data)
 
